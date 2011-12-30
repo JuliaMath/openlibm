9f7ab322bb12dcb9a3aae8e6c85d454baebe2691
Viral B. Shah <viral@mayin.org>
2011-12-31 01:10:24 +0530

    Makefile

--- src/build.clang.sh
+++
@@ -1,1 +0,0 @@
-for i in *.c; do clang -D__BSD_VISIBLE -Wno-implicit-function-declaration -c -O0 -I. -I../include -I../ld128 $i; done 2>err
--- src/build.gcc.sh
+++
@@ -1,1 +0,0 @@
-for i in *.c; do gcc -D__BSD_VISIBLE -c -O0 -I. -I../include -I../ld128 $i; done 2>err
