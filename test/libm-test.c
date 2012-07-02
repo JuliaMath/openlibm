5656ce42969852550d40460062e06aefd6f81aab
Viral B. Shah <viral@mayin.org>
2012-07-02 20:25:10 +0530

    Use the c99 standard rather than gnu99.

--- test/libm-test.c
+++ test/libm-test.c
@@ -4538,9 +4538,7 @@ main (int argc, char **argv)
   /* Nearest integer functions:  */
   ceil_test ();
   floor_test ();
-  /*
-  nearbyint_test ();
-  */
+  //nearbyint_test ();
   rint_test ();
   lrint_test ();
   llrint_test ();
