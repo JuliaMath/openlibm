abc6c5e76ffc6debfa704125efc8b1dc72ffbc32
Viral B. Shah <viral@mayin.org>
2012-08-19 17:03:16 +0530

    Revert to commit 87ce7ceddab2294d5e6f8d6c4c99837309757874 and disable the building of test-double-system and test-float-system by default

--- test/libm-test.c
+++ test/libm-test.c
@@ -224,7 +224,7 @@ static FLOAT max_error, real_max_error, imag_max_error;
 #define MANT_DIG CHOOSE ((LDBL_MANT_DIG-1), (DBL_MANT_DIG-1), (FLT_MANT_DIG-1),  \
                          (LDBL_MANT_DIG-1), (DBL_MANT_DIG-1), (FLT_MANT_DIG-1))
 
-#if defined(__WIN32__) || defined(__APPLE__)
+#ifndef SYS_MATH_H
 void FUNC(sincos) (int n, FLOAT *s, FLOAT *c)
 {
 	*s = FUNC(sin) ( *s );
