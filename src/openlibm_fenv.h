d07820351bed7d16f1f0a1ae0596a2e2b6f50aaf
Ed Schouten <ed@nuxi.nl>
2015-01-10 10:21:57 +0100

    Rename the fenv headers.
    I guess the idea would be to eventually also install all of the
    openlibm*.h headers, instead of just openlibm.h. Make openlibm_fenv.h
    suitable for this purpose by moving all of the $ARCH/fenv.h headers next
    to it.
    
    We actually need this change to make OPENLIBM_USE_HOST_FENV_H work.
    Right now it's still broken, because the "#include <fenv.h>" performed
    by openlibm_fenv.h still pulls in $ARCH/fenv.h as $ARCH/ is added to the
    compiler include path.

--- src/openlibm_fenv.h
+++ src/openlibm_fenv.h
@@ -3,11 +3,11 @@
 #else /* !OPENLIBM_USE_HOST_FENV_H */
 
 #if defined(__arm__)
-#include "../arm/fenv.h"
+#include <openlibm_fenv_arm.h>
 #elif defined(__x86_64__)
-#include "../amd64/fenv.h"
+#include <openlibm_fenv_amd64.h>
 #elif defined(__i386__)
-#include "../i387/fenv.h"
+#include <openlibm_fenv_i387.h>
 #else
 #error "Unsupported platform"
 #endif
