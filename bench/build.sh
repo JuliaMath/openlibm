effb7357f13dfaaff022a1c2f73d2be1c67265c1
Ondřej Čertík <ondrej@certik.us>
2017-11-15 23:27:09 -0700

    Add link time optimization

--- bench/build.sh
+++ bench/build.sh
@@ -7,10 +7,12 @@ FFLAGS="-Wall -Wextra -Wimplicit-interface -fPIC -g -fcheck=all -fbacktrace"
 FFLAGS="-Wall -Wextra -Wimplicit-interface -fPIC -O3 -march=native -ffast-math -funroll-loops"
 
 CFLAGS="-fno-gnu89-inline -fno-builtin -fPIC -m64 -std=c99 -Wall -O3"
+gcc -flto $CFLAGS -Wno-implicit-function-declaration -c redux.c -o redux.o
+
 #CFLAGS="$CFLAGS -march=native -ffast-math -funroll-loops"
 
-gcc $CFLAGS -I../include -DASSEMBLER -D__BSD_VISIBLE -Wno-implicit-function-declaration -c ../src/s_exp2.c -o s_exp2.c.o
-gfortran $FFLAGS -c test_exp2.f90 -o test_exp2.o
-gfortran $FFLAGS s_exp2.c.o test_exp2.o -o test_exp2
+gcc -flto $CFLAGS -I../include -DASSEMBLER -D__BSD_VISIBLE -Wno-implicit-function-declaration -c ../src/s_exp2.c -o s_exp2.c.o
+gfortran -flto $FFLAGS -c test_exp2.f90 -o test_exp2.o
+gfortran -flto $FFLAGS s_exp2.c.o test_exp2.o redux.o -o test_exp2
 
 ./test_exp2
