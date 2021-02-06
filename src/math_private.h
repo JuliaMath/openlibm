40dac9dd77e8d5429f14ce6f7cc7c44605542be6
Viral B. Shah <viral@mayin.org>
2021-02-06 17:27:15 -0500

    Restore STRICT_ASSIGN on FreeBSD as suggested in #215
    Co-authored-by: @kargl

--- src/math_private.h
+++ src/math_private.h
@@ -203,10 +203,9 @@ do {								\
 } while (0)
 
 
-//VBS
+#ifndef __FreeBSD__
 #define	STRICT_ASSIGN(type, lval, rval)	((lval) = (rval))
-
-/* VBS
+#else
 #ifdef FLT_EVAL_METHOD
 // Attempt to get strict C99 semantics for assignment with non-C99 compilers.
 #if FLT_EVAL_METHOD == 0 || __GNUC__ == 0
@@ -215,7 +214,7 @@ do {								\
 #define	STRICT_ASSIGN(type, lval, rval) do {	\
 	volatile type __lval;			\
 						\
-	if (sizeof(type) >= sizeof(double))	\
+	if (sizeof(type) >= sizeof(long double))	\
 		(lval) = (rval);		\
 	else {					\
 		__lval = (rval);		\
@@ -224,7 +223,7 @@ do {								\
 } while (0)
 #endif
 #endif
-*/
+#endif
 
 /*
  * Common routine to process the arguments to nan(), nanf(), and nanl().
